4c53ec1783c370da857a91e033761c4b1f6921e5
Mihir-patil-2242 <mihirpatil2242@gmail.com>
2025-06-18 00:49:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def generate_caption(topic):
 def create_meme_image(caption, uploaded_img, font_path="ComicNeue-Bold.ttf"):
     img = Image.open(uploaded_img)
     img_width = img.width
-    size = int(img_width / 15)
+    size = 32
 
     try:
         font = ImageFont.truetype(font_path, size)
